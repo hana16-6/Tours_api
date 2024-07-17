5cb4e031eccc714b66afa154362e3a2b1e6271a9
hana16-6 <64643374+hana16-6@users.noreply.github.com>
2024-07-17 11:37:31 +0300

    add globalglobalErrorHandler middleware

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utilities/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/toursRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -13,5 +15,11 @@ app.use(express.json());
 //routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find this ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
