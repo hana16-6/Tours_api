e09441fd6669ec3498400d7a969cf6e7460349ac
hana16-6 <64643374+hana16-6@users.noreply.github.com>
2024-10-29 11:47:09 +0300

    add protect routes

--- routes/reviewsRoutes.js
+++ routes/reviewsRoutes.js
@@ -4,18 +4,26 @@ const authController = require('../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
 
+router.use(authController.protectRoutes);
 router
   .route('/')
   .get(reviewsController.getAllReviews)
   .post(
-    authController.protectRoutes,
     authController.restrictTo('user'),
+    reviewsController.setTourUserIds,
     reviewsController.createReview,
   );
 
 router
   .route('/:id')
-  .patch(reviewsController.updateReview)
-  .delete(reviewsController.deleteReview);
+  .get(reviewsController.getReview)
+  .patch(
+    authController.restrictTo('user', 'admin'),
+    reviewsController.updateReview,
+  )
+  .delete(
+    authController.restrictTo('user', 'admin'),
+    reviewsController.deleteReview,
+  );
 
 module.exports = router;
