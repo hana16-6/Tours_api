5f1c93c5fc36b9b29f6aabcdf59da6dc2e461b9d
hana16-6 <64643374+hana16-6@users.noreply.github.com>
2024-10-29 11:08:41 +0300

    add git add function

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,3 +1,4 @@
+const APIFeatures = require('../utilities/apiFeatures');
 const AppError = require('../utilities/appError');
 const catchAsync = require('../utilities/catchAsync');
 
@@ -32,9 +33,47 @@ exports.updateOne = (Model) =>
 
 exports.createOne = (Model) =>
   catchAsync(async (req, res, next) => {
-    const newTour = await Model.create(req.body);
+    const doc = await Model.create(req.body);
     res.status(201).json({
       status: 'success',
-      data: newTour,
+      data: doc,
+    });
+  });
+
+exports.getOne = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+    if (popOptions) query = query.populate(popOptions);
+    const doc = await query;
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    // to allow for nested GET reviews on tour
+    let filter = {};
+    if (req.params.tourId) filter = { tour: req.params.tourId };
+    //execute query
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const doc = await features.query;
+
+    res.status(200).json({
+      status: 'success',
+      results: doc.length,
+      data: {
+        data: doc,
+      },
     });
   });
