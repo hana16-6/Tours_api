634d87dda15c18c30918abc4aba79655f47195bc
hana16-6 <64643374+hana16-6@users.noreply.github.com>
2024-10-30 20:15:20 +0300

    prevent review duplication

--- models/reviewsModel.js
+++ models/reviewsModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -31,6 +32,7 @@ const reviewSchema = new mongoose.Schema(
     toObject: { virtuals: true },
   },
 );
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
 reviewSchema.pre(/^find/, function (next) {
   // this.populate({
   //   path: 'tour',
@@ -46,6 +48,43 @@ reviewSchema.pre(/^find/, function (next) {
   next();
 });
 
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
+};
+reviewSchema.post('save', function () {
+  //this keyword points to current review // current document
+  this.constructor.calcAverageRatings(this.tour);
+});
+reviewSchema.pre('/^findOneAnd/', async function (next) {
+  //this keyword points to current query
+  this.rev = await this.findOne();
+  next();
+});
+reviewSchema.post('/^findOneAnd/', async function () {
+  await this.constructor.calcAverageRatings(this.rev.tour);
+});
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
