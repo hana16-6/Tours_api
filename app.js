b31bacc51447505afd87f559775f326403ac18f7
hana16-6 <64643374+hana16-6@users.noreply.github.com>
2024-11-12 22:27:58 +0200

    add pug view engine

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -13,8 +14,12 @@ const userRouter = require('./routes/userRoutes');
 const reviewsRouter = require('./routes/reviewsRoutes');
 
 const app = express();
-// 1) GLOBAL MIDDLEWARES
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
+// 1) GLOBAL MIDDLEWARES
+//serving static files
+app.use(express.static(path.join(__dirname, 'public')));
 //set security HTTP headers
 app.use(helmet());
 
@@ -53,9 +58,6 @@ app.use(
   }),
 );
 
-//serving static files
-app.use(express.static(`${__dirname}/public`));
-
 //test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -63,6 +65,10 @@ app.use((req, res, next) => {
 });
 
 //routes
+
+app.get('/', (req, res) => {
+  res.status(200).render('base');
+});
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewsRouter);
