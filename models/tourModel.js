b8485382d11340742c03351d5e6a08abae22b4a5
hana16-6 <64643374+hana16-6@users.noreply.github.com>
2024-11-20 22:00:39 +0200

    edit location property to locations

--- models/tourModel.js
+++ models/tourModel.js
@@ -90,7 +90,7 @@ const tourSchema = new mongoose.Schema(
       address: String,
       description: String,
     },
-    location: [
+    locations: [
       {
         type: {
           type: String,
