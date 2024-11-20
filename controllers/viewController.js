2563de323379be901bac4d7ee04184a23849b5dc
hana16-6 <64643374+hana16-6@users.noreply.github.com>
2024-11-20 22:03:31 +0200

    add overview and get tour controllers

---
+++ controllers/viewController.js
@@ -0,0 +1,28 @@
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utilities/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
+
+  res.status(200).render('overview', {
+    title: 'All Tours',
+    tours,
+  });
+});
+exports.getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+  res.status(200).render('tour', {
+    title: tour.name,
+    tour,
+  });
+});
+
+exports.getLoginForm = catchAsync(async (req, res, next) => {
+  res.status(200).render('login', {
+    title: 'Login',
+  });
+});
